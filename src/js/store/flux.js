9676d37da24ba9ac6bd14cca5c804cbd828e6bb0
David Lozano <dloznao@gmail.com>
2020-04-14 01:03:59 +0000

    added fetch

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,15 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			randomQuote: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			updateRandomQuote: () => {
+				fetch("https://type.fit/api/quotes")
+					.then(resp => resp.json())
+					.then(data => {
+						console.log(data);
+					});
 			}
 		}
 	};
